4c636c72259b66314d470f657cc41338ef3ffbd6
AndrijaVarga <andrija.varga026@gmail.com>
2021-03-21 15:25:56 +0100

    Major updates
    Added simple translation and changed the navigation design.

---
+++ languages.js
@@ -0,0 +1,32 @@
+//Define language reload anchors
+var dataReload = document.querySelectorAll("[data-reload]");
+//Language translations
+var language = {
+    cro: {
+        home: "Početna"
+    },
+    eng: {
+        home: "Home"
+    },
+    it:{
+        home: "Casa"
+    }
+};
+//Define language via window hash
+if(window.location.hash){
+    if(window.location.hash === "#cro"){
+        homeLink.textContent = language.cro.home;
+    }
+    if(window.location.hash === "#eng"){
+        homeLink.textContent = language.eng.home;
+    }
+    if(window.location.hash === "#it"){
+        homeLink.textContent = language.it.home;
+    }
+}
+//define language reload onClick illiteration
+/*for (i=0; i<=dataReload.length; i++){
+    dataReload[i].onclick=function(){
+        location.reload(true);
+    };
+}*/
